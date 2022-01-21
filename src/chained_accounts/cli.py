e5de31937cac1d20cdebf311c838f0456b15b3ec
clamdad <6515522+clamdad@users.noreply.github.com>
2022-01-21 07:00:56 -0500

    Terminology and docs

--- src/chained_accounts/cli.py
+++ src/chained_accounts/cli.py
@@ -21,56 +21,64 @@ def main(ctx, version) -> None:
 
 
 @main.command()
-@click.argument("name", type=str)
+@click.argument("uid", type=str)
 @click.argument("private_key", type=str)
 @click.argument("chain_ids", type=int, nargs=-1)
-def add(name: str, private_key: str, chain_ids: List[int]) -> None:
+def add(uid: str, private_key: str, chain_ids: List[int]) -> None:
     """Add an account.
 
-    Adds an encrypted private_key for use by an application.  ACCOUNT_NAME is a unique name
-    used to refer to the account.  The PRIVATE_KEY should be provided in hexadecimal format,
-    beginning with 0x.  CHAIN_IDS is a list of the chains on which applications may use this account.
-    If PASSWORD is not provided, the user will be prompted to enter a password
+    Adds an ethereum account for use by an application on one or more EVM chains.
+    UID (unique account ID) is any convenient string used to identify the account.
+    The PRIVATE_KEY should be provided in hexadecimal format, beginning with 0x.
+    CHAIN_IDS is a list of the chains on which applications may use this account.
     """
 
-    test_account = ChainedAccount(name)
+    test_account = ChainedAccount(uid)
     if test_account.keyfile.exists():
-        click.echo(f"Account {name} already exists.")
+        click.echo(f"Account {uid} already exists.")
         return
 
-    account = ChainedAccount.add(name=name, private_key=HexBytes(private_key), chain_ids=chain_ids)
+    account = ChainedAccount.add(uid, private_key=HexBytes(private_key), chain_ids=chain_ids)
 
-    click.echo(f"Added new account {name} (address= {account.address}) for use on chains {account.chain_ids}")
+    click.echo(f"Added new account {uid} (address= {account.address}) for use on chains {account.chain_ids}")
 
 
 @main.command()
-@click.option("--name", type=str)
+@click.option("--uid", type=str)
 @click.option("--address", type=str)
 @click.option("--chain_id", type=int)
-def find(name, address, chain_id):
-    """Find accounts."""
+def find(uid, address, chain_id):
+    """Search the keystore for accounts.
 
-    accounts = find_accounts(name=name, address=address, chain_id=chain_id)
+    Each option is used as a filter when searching the keystore.
+    If no options are provided, all accounts will be returned.
+    """
+
+    accounts = find_accounts(uid=uid, address=address, chain_id=chain_id)
     click.echo(f"Found {len(accounts)} accounts.")
     for account in accounts:
-        click.echo(f"Account name: {account.name}, address: {account.address}, chain IDs: {account.chain_ids}")
+        click.echo(f"Account uid: {account.uid}, address: {account.address}, chain IDs: {account.chain_ids}")
 
 
 @main.command()
-@click.argument("name", type=str)
+@click.argument("uid", type=str)
 @click.option("-p", "--password", type=str)
-def key(name, password):
-    """Get the private key for an account."""
+def key(uid, password):
+    """Get the private key for an account.
 
-    account = ChainedAccount(name)
+    UID is the account id used to create the account.
+    User will be prompted for the password if it is not provided through the command line option.
+    """
+
+    account = ChainedAccount(uid)
     if not account.keyfile.exists():
-        click.echo(f"Account {name} does not exist.")
+        click.echo(f"Account {uid} does not exist.")
         return
 
     try:
         if not password:
-            password = getpass.getpass(f"Enter password for {name} keyfile: ")
-        acc = ChainedAccount(name)
+            password = getpass.getpass(f"Enter password for {uid} keyfile: ")
+        acc = ChainedAccount(uid)
         acc.unlock(password)
         click.echo(f"Private key: {acc.private_key.hex()}")
     except ValueError:
@@ -78,13 +86,15 @@ def key(name, password):
 
 
 @main.command()
-@click.argument("name", type=str)
-def delete(name):
-    """Delete an account."""
+@click.argument("uid", type=str)
+def delete(uid):
+    """Delete an account from the keystore.
 
-    account = ChainedAccount(name)
+    UID is the account id used to create the account.
+    """
+    account = ChainedAccount(uid)
     if not account.keyfile.exists():
-        click.echo(f"Account {name} does not exist.")
+        click.echo(f"Account {uid} does not exist.")
         return
 
     account.delete()
--- tests/test_chained_account.py
+++ tests/test_chained_account.py
@@ -11,8 +11,8 @@ pkey = HexBytes("0x7a28b5ba57c53603b0b07b56bba752f7784bf506fa95edc395f5cf6c7514f
 PASSWORD = "foo"
 
 
-def random_name() -> str:
-    """Generate a random account name."""
+def random_uid() -> str:
+    """Generate a random account uid."""
     return "temp" + str(random.randint(100000, 999999))
 
 
@@ -20,9 +20,9 @@ def random_name() -> str:
 def test_accounts() -> List[ChainedAccount]:  # type: ignore
     """Create some test accounts"""
 
-    a1 = ChainedAccount.add(random_name(), chain_ids=[1, 4], private_key=pkey, password=PASSWORD)
-    a2 = ChainedAccount.add(random_name(), chain_ids=[2], private_key=pkey, password=PASSWORD)
-    a3 = ChainedAccount.add(random_name(), chain_ids=[1, 3], private_key=pkey, password=PASSWORD)
+    a1 = ChainedAccount.add(random_uid(), chain_ids=[1, 4], private_key=pkey, password=PASSWORD)
+    a2 = ChainedAccount.add(random_uid(), chain_ids=[2], private_key=pkey, password=PASSWORD)
+    a3 = ChainedAccount.add(random_uid(), chain_ids=[1, 3], private_key=pkey, password=PASSWORD)
 
     yield [a1, a2, a3]
 
@@ -40,8 +40,8 @@ def test_new_evm_account(test_accounts):
     assert 1 in test_accounts[0].chain_ids
     assert 4 in test_accounts[0].chain_ids
 
-    # Test loading by name
-    account_copy = ChainedAccount(test_accounts[0].name)
+    # Test loading by uid
+    account_copy = ChainedAccount(test_accounts[0].uid)
     assert 1 in account_copy.chain_ids
     assert 4 in account_copy.chain_ids
     assert account_copy.address == test_accounts[0].address
@@ -57,15 +57,15 @@ def test_new_evm_account(test_accounts):
 
     # Verify private keys (including CLI)
     assert test_accounts[0].private_key == test_accounts[1].private_key
-    result = runner.invoke(main, ["key", test_accounts[0].name, "-p", PASSWORD])
+    result = runner.invoke(main, ["key", test_accounts[0].uid, "-p", PASSWORD])
     assert pkey.hex() in result.stdout
 
     # Lock account and verify
     test_accounts[0].lock()
     assert not test_accounts[0].unlocked
 
-    # Create a new account objecb by name
-    t1 = ChainedAccount.get(test_accounts[0].name)
+    # Create a new account objecb by uid
+    t1 = ChainedAccount.get(test_accounts[0].uid)
     assert isinstance(t1, ChainedAccount)
 
     # Search account
@@ -74,9 +74,9 @@ def test_new_evm_account(test_accounts):
     result = runner.invoke(main, ["find", "--address", "0x008aeeda4d805471df9b2a5b0f38a0c3bcba786b"])
     assert "Found 3 accounts" in result.stdout
 
-    results = find_accounts(name=test_accounts[2].name)
+    results = find_accounts(uid=test_accounts[2].uid)
     assert len(results) == 1
-    result = runner.invoke(main, ["find", "--name", test_accounts[2].name])
+    result = runner.invoke(main, ["find", "--uid", test_accounts[2].uid])
     assert "Found 1 accounts" in result.stdout
 
     results = find_accounts(chain_id=1)
